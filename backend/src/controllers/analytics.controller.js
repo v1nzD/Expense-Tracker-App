efe02580b09f52a11f3258dc4451554de3faa43d
Vince <vincedimatatac@gmail.com>
2026-04-27 21:39:29 +0800

    feat: added expense summary route and controller function

---
+++ backend/src/controllers/analytics.controller.js
@@ -0,0 +1,32 @@
+import pool from "../config/db.js";
+
+export async function getExpenseSummary(req, res) {
+  try {
+    const { user_id } = req.query;
+
+    if (!user_id) {
+      return res.status(400).json({ error: "Missing user ID" });
+    }
+
+    const queryTotal =
+      "SELECT COALESCE(SUM(amount), 0) AS total_spent FROM expenses WHERE user_id = $1";
+
+    const queryCategoryTotal = `SELECT c.name, COALESCE(SUM(e.amount), 0) AS total
+       FROM expenses e
+       LEFT JOIN categories c ON e.category_id = c.id
+       WHERE e.user_id = $1
+       GROUP BY c.name
+       ORDER BY total DESC`;
+
+    const totalResult = await pool.query(queryTotal, [user_id]);
+    const categoryResult = await pool.query(queryCategoryTotal, [user_id]);
+
+    return res.status(200).json({
+      total_spent: totalResult.rows[0].total_spent,
+      by_category: categoryResult.rows,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error getting expense summary" });
+  }
+}
--- backend/src/routes/expense.route.js
+++ backend/src/routes/expense.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addExpense, getExpenses } from "../controllers/expense.controller.js";
+import { getExpenseSummary } from "../controllers/analytics.controller.js";
 
 const expenseRouter = Router();
 
 expenseRouter.post("/", addExpense);
 expenseRouter.get("/", getExpenses);
+expenseRouter.get("/summary", getExpenseSummary);
 
 export default expenseRouter;
