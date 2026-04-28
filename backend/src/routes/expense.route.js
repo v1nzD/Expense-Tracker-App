d92f313291de52618b8122187fa391366562f8d7
Vince <vincedimatatac@gmail.com>
2026-04-28 21:51:38 +0800

    added protectRoute for expense routes

--- backend/src/routes/expense.route.js
+++ backend/src/routes/expense.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { addExpense, getExpenses } from "../controllers/expense.controller.js";
 import { getExpenseSummary } from "../controllers/analytics.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const expenseRouter = Router();
+
+expenseRouter.use(protectRoute);
 
 expenseRouter.post("/", addExpense);
 expenseRouter.get("/", getExpenses);
