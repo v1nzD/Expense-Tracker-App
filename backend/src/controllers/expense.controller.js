ed75b60c9fc5f439311c51cde23d3c9ccc247bec
Vince <vincedimatatac@gmail.com>
2026-04-29 12:18:46 +0800

    feat: added pagination for expenses

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -2,10 +2,10 @@ import pool from "../config/db.js";
 
 export async function addExpense(req, res) {
   try {
-    const { user_id, amount, category_id, description, expense_date } =
-      req.body;
+    const user_id = req.user.id;
+    const { amount, category_id, description, expense_date } = req.body;
 
-    if (!user_id || !amount || !expense_date) {
+    if (!amount || !expense_date) {
       return res.status(400).json({ error: "Missing required fields" });
     }
 
@@ -23,23 +23,29 @@ export async function addExpense(req, res) {
 
 export async function getExpenses(req, res) {
   try {
-    const { user_id, category_id, start_date, end_date } = req.query;
-
-    // validate user
-    if (!user_id) {
-      return res.status(400).json({ error: "User ID is required" });
-    }
+    const user_id = req.user.id;
+    const {
+      category_id,
+      start_date,
+      end_date,
+      page = 1,
+      limit = 10,
+    } = req.query;
 
     const userId = parseInt(user_id);
     const categoryId = category_id ? parseInt(category_id) : null;
 
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+    const offset = (pageNumber - 1) * limitNumber;
+
     let query = "SELECT * FROM expenses WHERE user_id = $1";
-    let values = [user_id];
+    let values = [userId];
     let index = 2;
 
     if (category_id) {
       query += ` AND category_id = $${index}`;
-      values.push(category_id);
+      values.push(categoryId);
       index++;
     }
 
@@ -57,9 +63,26 @@ export async function getExpenses(req, res) {
 
     query += " ORDER BY expense_date DESC";
 
+    // add pagination
+    query += ` LIMIT $${index} OFFSET $${index + 1}`;
+    values.push(limitNumber, offset);
+
     const result = await pool.query(query, values);
 
-    return res.status(200).json(result.rows);
+    // total count
+    const countResult = await pool.query(
+      "SELECT COUNT(*) FROM expenses where user_id = $1",
+      [userId],
+    );
+    // let countQuery = "SELECT COUNT(*) FROM expenses WHERE user_id = $1";
+    // let countValues = [userId];
+
+    return res.status(200).json({
+      page: pageNumber,
+      limit: limitNumber,
+      total: parseInt(countResult.rows[0].count),
+      data: result.rows,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Error gettting expenses" });
