0c6c3487cf33be92019672bc6c13c0b7699f5316
Vince <vincedimatatac@gmail.com>
2026-04-29 12:16:09 +0800

    fix: removed auth middleware from auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,7 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/auth.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
 
 const authRouter = Router();
-
-authRouter.use(protectRoute);
 
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
