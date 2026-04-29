efebde6f601fe866cd95a8b156f5c50887afd13f
Vince <vincedimatatac@gmail.com>
2026-04-29 12:16:33 +0800

    fix: modified auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,32 +1,22 @@
 import jwt from "jsonwebtoken";
 import { ENV } from "../config/env.js";
 
-export const protectRoute = async (req, res, next) => {
+export const protectRoute = (req, res, next) => {
   try {
-    // get token from header
-    const authHeader = req.headers["authorization"];
+    const authHeader = req.headers.authorization;
+
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ error: "Unathorized" });
+      return res.status(401).json({ error: "Unauthorized" });
     }
 
     const token = authHeader.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ error: "No token provided" });
-    }
 
-    // verify token
-    jwt.verify(token, ENV.JWT_SECRET, (err, decodedUser) => {
-      if (err) {
-        return res.status(403).json({ error: "Invalid or expired token" });
-      }
+    const decodedUser = jwt.verify(token, ENV.JWT_SECRET);
 
-      // attach user info to request and proceed
-      req.user = decodedUser;
+    req.user = decodedUser;
 
-      next();
-    });
+    next();
   } catch (error) {
-    console.error("Error in protectRoute middleware", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(401).json({ error: "Invalid or expired token" });
   }
 };
