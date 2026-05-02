1b096b5701d2c766e4933fd420d74241877ce358
Vince <vincedimatatac@gmail.com>
2026-05-02 19:56:40 +0800

    client initial setup + api axios setup

---
+++ client/src/api/client.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const API_URL = "http://192.168.1.47/api";
+
+// Axios instance
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+// Request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      // get token from seucre storage
+      const token = await SecureStore.getItemAsync("token");
+
+      if (token) {
+        // attach authorization header
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    } catch (error) {
+      console.error("Error getting token:", error);
+      return config;
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
